b30464fe68e57304d17d0615684c11ed27545baf
Zero'F_Fa <hyperegex@163.com>
2026-09-21 04:27:23 +0800

    content: add asterisk superscript to Feng Shan

--- app/page.tsx
+++ app/page.tsx
@@ -168,7 +168,7 @@ function Publication() {
 
                     <p className="text-neutral-600 dark:text-neutral-400">
                         <strong>Baixin Wan</strong>, Haozhe Zhang, Jiaqi Li,
-                        Jianping Huang, Feng Shan, and Yun Wang.
+                        Jianping Huang, Feng Shan<sup>*</sup>, and Yun Wang.
                     </p>
 
                     <p className="text-sm text-neutral-500 dark:text-neutral-400">
@@ -189,7 +189,8 @@ function Publication() {
                     </p>
 
                     <p className="text-neutral-600 dark:text-neutral-400">
-                        <strong>Baixin Wan</strong>, Feng Shan, Jianping Huang.
+                        <strong>Baixin Wan</strong>, Feng Shan<sup>*</sup>,
+                        Jianping Huang.
                     </p>
 
                     <p className="text-sm text-neutral-500 dark:text-neutral-400">
