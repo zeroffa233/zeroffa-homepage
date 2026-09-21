f0a06293913b78c3dd6a4e8f4b792f478d71f5ae
Zero'F_Fa <hyperegex@163.com>
2026-09-22 03:58:06 +0800

    style: blog list entry spacing unified to mb-2

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,7 +18,7 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4 group"
+            className="flex flex-col space-y-1 mb-2 group"
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-row items-baseline space-x-2">
