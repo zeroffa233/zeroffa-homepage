208cad3f3190feabd8f3929278ef65e1e70347ec
Zero'F_Fa <hyperegex@163.com>
2026-09-21 11:17:15 +0800

    content: My Homepage / Welcome to my homepage across metadata, JSON-LD, RSS

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -82,7 +82,7 @@ export default async function Blog({ params }) {
                         url: `${baseUrl}/blog/${post.slug}`,
                         author: {
                             "@type": "Person",
-                            name: "My Portfolio",
+                            name: "Baixin Wan",
                         },
                     }),
                 }}
