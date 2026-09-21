6eaa608c4aa1083c3c66d81e4bedf5737587c7ca
Zero'F_Fa <hyperegex@163.com>
2026-09-21 19:05:10 +0800

    style: notes list bullets instead of numbers

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,7 +4,7 @@ import { getNotesStructure, NoteListItem } from "app/notes/utils";
 
 function EntryList({ notes }: { notes: NoteListItem[] }) {
     return (
-        <ol className="list-decimal list-outside pl-5 text-lg">
+        <ul className="list-disc list-outside pl-5 text-lg">
             {notes.map((note) => (
                 <li key={note.slug} className="mb-2">
                     <div className="flex justify-between items-baseline">
@@ -22,7 +22,7 @@ function EntryList({ notes }: { notes: NoteListItem[] }) {
                     </div>
                 </li>
             ))}
-        </ol>
+        </ul>
     );
 }
 
